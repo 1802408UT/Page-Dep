be917a8d4ad1306d935c147c06dfabc385f2363c
1802808UT <1802408@utrivieramaya.edu.mx>
2021-06-17 19:17:47 -0500

    update

--- Page-Dep/src/app/app.module.ts
+++ Page-Dep/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BodyComponent } from './component/body/body.component';
 import { AboutComponent } from './component/body/about/about.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
