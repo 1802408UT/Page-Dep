827c97f9b02597390cfc5f4d45d0e7896ef3eb96
1802808UT <1802408@utrivieramaya.edu.mx>
2021-06-17 19:04:44 -0500

    update

--- Page-Dep/src/app/app.module.ts
+++ Page-Dep/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BodyComponent } from './component/body/body.component';
+import { AboutComponent } from './component/body/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
